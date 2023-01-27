51445d6771b00b99467e5c5b39030af377ef14fe
Marc Blumenauer <marc.blumenauer@kobuleti.de>
2023-01-27 21:02:23 +0100

    update

--- orno.py
+++ orno.py
@@ -105,7 +105,7 @@ class orno:
       if self.type == 0:
         return self.smartmeter.read_register(register,decimals,3)
       elif self.type == 1:
-        return self.smartmeter.read_float(register,decimals)
+        return self.smartmeter.read_float(register,num=decimals)
 
   def read_float(self, register=0, num=1, code=3, order=0):
       return self.smartmeter.read_float(register,code,num,order)
