a1dc0c03bc093e47ddf1b63062d7cb8c20aae158
Marc Blumenauer <marc.blumenauer@kobuleti.de>
2023-01-27 22:30:19 +0100

    add

--- orno.py
+++ orno.py
@@ -185,7 +185,8 @@ class orno:
       print(self.txt.format(TP=self.TotalPower))
     elif self.type == 1:
       print(f"L1 Voltage        {self.L1_voltage:.0f} V")
-
+      print(f"L1 Frequency      {self.L1_frequency:.2f} Hz")
+      
   def doLoop(self, count=0, infinite=True):
     if self.useMQTT and not self.isMQTT_connected:
       self.mqtt_enable()
