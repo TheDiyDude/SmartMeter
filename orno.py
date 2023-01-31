b628e9de1c16e158f1fcccf0bc054da31fa40157
TheDiyDude <marc@l3c.de>
2023-01-31 16:56:45 +0100

    fixed incorrect slave id building

--- orno.py
+++ orno.py
@@ -365,7 +365,7 @@ class orno:
   def mqtt_enable(self):
     #self.mqtt_client_id=f'ORNO-{random.randint(1000, 8000)}'
     if self.mqtt_client_id == "":
-      self.mqtt_client_id=f"ORNO-{socket.gethostname()}-{random.randint(1000, 8000)"
+      self.mqtt_client_id=f"ORNO-{socket.gethostname()}-{random.randint(1000, 8000)}"
     self.mqtt_prepareTopics(self.type)
     if self.debug:
       self.logMessage(f"Using MQTT Broker: '{self.mqtt_broker}:{self.mqtt_port}' with user '{self.mqtt_username}' and secret '{self.mqtt_password}'")
